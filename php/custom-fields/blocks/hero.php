a755e7696f3bae0782893d730089edf4717a2d75
Haroon Yamin <haroon.webdev@gmail.com>
2025-05-21 15:53:09 +0500

    #

--- php/custom-fields/blocks/hero.php
+++ php/custom-fields/blocks/hero.php
@@ -1,3 +1,14 @@
-<section>
-    
+<?php
+    $label = get_field('content')['label'];
+    $heading = get_field('content')['heading'];
+    $paragraph = get_field('content')['paragraph'];
+    $bg_image = get_field('more')['bg_image'];
+    $button = get_field('more')['button'];
+    $shortcode = get_field('more')['shortcode'];
+?>
+
+<section style="background: url(<?=  $bg_image; ?>) center center/ cover no-repeat;">
+    <div class="container" style="height: 600px">
+
+    </div>
 </section>
