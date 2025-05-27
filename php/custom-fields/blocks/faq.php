ae10ede9883e21c977504e75d617ed5697d20002
Haroon Yamin <haroon.webdev@gmail.com>
2025-05-27 14:21:34 +0500

    FAQ responsive

--- php/custom-fields/blocks/faq.php
+++ php/custom-fields/blocks/faq.php
@@ -3,7 +3,7 @@
     $repeater = get_field('repeater', 'option');
 ?>
 
-<section class="my-section-lg">
+<section class="lg:my-section-lg my-12">
     <div class="container mx-auto px-6">
         <?php if( $heading ) {
             heading_2( $heading, 'text-center mx-auto' );
@@ -17,10 +17,10 @@
                     $unique_id = 'faq-' . $index; ?>
 
                     <div class="bg-white border-b border-gray-200">
-                        <button class="accordion-trigger w-full py-4 text-left flex justify-between items-center hover:bg-gray-50 text-xl cursor-pointer" 
+                        <button class="accordion-trigger w-full py-4 text-left flex gap-x-4 justify-between items-center hover:bg-gray-50 md:text-xl text-lg cursor-pointer" 
                                 data-target="<?php echo $unique_id; ?>">
                             <span class="text-gray-800 font-medium"><?php echo esc_html($question); ?></span>
-                            <div class="accordion-icon w-8 h-8 bg-light-green rounded-full flex items-center justify-center relative">
+                            <div class="accordion-icon min-w-8 w-8 h-8 bg-light-green rounded-full flex items-center justify-center relative">
                                 <div class="w-4 h-cross bg-white absolute"></div><div class="w-4 h-cross bg-white absolute rotate-90"></div>
                             </div>
                         </button>
