2fd8c73186acfa96a2bb1b6b3a977186aa4b8031
Haroon Yamin <haroon.webdev@gmail.com>
2025-05-27 14:34:36 +0500

    form section responsive

--- php/custom-fields/blocks/form.php
+++ php/custom-fields/blocks/form.php
@@ -5,17 +5,17 @@
     $shortcode = get_field('shortcode');
 ?>
 
-<section style="background: url(<?=  $bg_image; ?>) center center/ cover no-repeat;" class="h-780">
+<section style="background: url(<?=  $bg_image; ?>) center center/ cover no-repeat;" class="md:h-780 py-12">
     <div class="container mx-auto h-full px-5">
-        <div class="grid grid-cols-2 gap-8 h-full items-center">
+        <div class="grid md:grid-cols-2 grid-cols-1 gap-x-8 gap-y-12 h-full items-center">
             <div class="block">
                 <div class="max-w-md">
                     <?php
                         if( $heading ) {
-                            echo '<h1 class="font-avenir mb-5 text-6xl font-bold text-white leading-2 max-w-xl">' . $heading . '</h1>';
+                            echo '<h1 class="font-avenir mb-5 md:text-6xl text-4xl font-bold text-white leading-2 max-w-xl">' . $heading . '</h1>';
                         }
                         if( $button ) {
-                            echo '<a href="' . $button['url'] . '" class="mt-7 bg-light-green border border-light-green py-3 px-8 text-white rounded-full hover:bg-lime-500 hover:border-lime-500 transition-colors inline-block" target="' . $button['target'] . '">' . $button['title'] . '</a>';
+                            echo button_1($button['url'], $button['target'], $button['title']);
                         }
                     ?>  
                 </div>
