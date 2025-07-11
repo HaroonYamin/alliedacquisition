543772b4fe8ad4f71f3ba18d96fac8ef21a4253a
Haroon Yamin <haroon.webdev@gmail.com>
2025-07-11 15:51:01 +0500

    Theme form removed

--- functions.php
+++ functions.php
@@ -50,11 +50,6 @@ require_once THEME_DIR . '/php/theme-settings/enqueue.php';
 require_once THEME_DIR . '/php/custom-fields/config.php';
 
 /*
- * Custom Form
- */
-require_once THEME_DIR . '/php/custom-form/main.php';
-
-/*
  * Custom Post Types
  */
 // require_once THEME_DIR . '/php/custom-post-types/config.php';
