41945c2fb701646f659d4bbf753e084dfe90c1d8
Haroon Yamin <haroon.webdev@gmail.com>
2025-08-17 08:23:31 +0500

    #

--- template-parts/header/bottom-bar.php
+++ template-parts/header/bottom-bar.php
@@ -3,7 +3,7 @@
         <a href="<?php echo esc_url( get_theme_mod( 'header_banner_url' ) ); ?>" class="bg-light-green hover:bg-neutral-700 block py-1 hover:py-2 transition-all">
             <div class="container mx-auto px-5">
                 <p class="text-white text-lg font-semibold text-center">
-                    <?= get_theme_mod( 'header_banner_content' ); ?>
+                    <?php echo get_theme_mod( 'header_banner_content' ); ?>
                 </p>
             </div>
         </a>
