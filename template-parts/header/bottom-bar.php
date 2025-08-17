3e55e3d79c0d292497bce39e6ad80356db6ce42a
Haroon Yamin <haroon.webdev@gmail.com>
2025-08-17 08:24:54 +0500

    #

--- template-parts/header/bottom-bar.php
+++ template-parts/header/bottom-bar.php
@@ -1,4 +1,4 @@
-<?php if ( get_theme_mod( 'header_banner_visibility', true ) || get_theme_mod( 'header_banner_content' ) ) : ?>
+<?php if ( get_theme_mod( 'header_banner_visibility', true ) && get_theme_mod( 'header_banner_content' ) ) : ?>
     <section class="fixed left-0 bottom-0 z-20 w-full shadow-banner" id="fixed-banner">
         <a href="<?php echo esc_url( get_theme_mod( 'header_banner_url' ) ); ?>" class="bg-light-green hover:bg-neutral-700 block py-1 hover:py-2 transition-all">
             <div class="container mx-auto px-5">
